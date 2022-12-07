d01a38151c5a31c362fdb4d881627d937917fe84
John Edvard Reiten <johnedvard89@gmail.com>
2022-12-07 21:07:21 +0900

    feat: add support to update levels completed state

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,32 @@
-import { useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { initGame } from '../public/lib/game.js';
 
 export default function Home() {
   const isGameInitialized = useRef(false);
   const [deathCount, setDeathCount] = useState(0);
+  const [levelsCompleted, setLevelsCompleted] = useState({});
+
+  const updateLevelsCompleted = useCallback(
+    (levels: { [key: string]: boolean }) => {
+      setLevelsCompleted((state) => ({ ...state, ...levels }));
+    },
+    []
+  );
+
   useEffect(() => {
     if (!isGameInitialized.current) {
       isGameInitialized.current = true;
-      initGame({ deathCount, setDeathCount });
+      initGame({ deathCount, setDeathCount, updateLevelsCompleted });
     }
-  }, [deathCount]);
+  }, [deathCount, updateLevelsCompleted]);
 
   return (
     <div>
-      <span>Death count: {deathCount} (Displayed on Next.js website)</span>
+      <div>Death count: {deathCount} (Displayed on Next.js website)</div>
+      <div>
+        Level completed: {JSON.stringify(levelsCompleted)} (Displayed on Next.js
+        website)
+      </div>
       <div id='hang-by-a-thread'></div>
     </div>
   );
